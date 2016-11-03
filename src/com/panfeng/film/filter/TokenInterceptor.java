b7128a9125590593e0dfc0036a70c1ccddcbf0a0
laowang <1300792971@qq.com>
2016-11-03 11:19:44 +0800

    lw

--- src/com/panfeng/film/filter/TokenInterceptor.java
+++ src/com/panfeng/film/filter/TokenInterceptor.java
@@ -4,34 +4,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.panfeng.film.service.PreventRepeat;
 import com.panfeng.film.util.DataUtil;
 import com.panfeng.film.util.ValidateUtil;
 
 /**
- * URL拦截器
- * 添加token访问机制
+ * URL拦截器 添加token访问机制
+ * 
  * @author Jack
  *
  */
 public class TokenInterceptor extends HandlerInterceptorAdapter {
 
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		
+	@Autowired
+	PreventRepeat preventRepeat;
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
 		// 获取session
 		final HttpSession session = request.getSession();
 		// 获取session的token
 		String stoken = (String) session.getAttribute("csrftoken");
-		
-		if(!ValidateUtil.isValid(stoken)){
+
+		if (!ValidateUtil.isValid(stoken)) {
 			// 如果没有，则创建
 			stoken = DataUtil.getUuid();
 			session.setAttribute("csrftoken", stoken);
 		}
-		
+
+		// add by Lw,2016-06-21 19:45 begin
+		// to prevent the repeat
+		// String rtoken = (String) session.getAttribute("rtoken");
+		// if (!ValidateUtil.isValid(stoken)) {
+		// // 如果没有，则创建
+		// stoken = DataUtil.getUuid();
+		// //session.setAttribute("rtoken", stoken);
+		//
+		//
+		// }
 		return true;
 	}
-	
+
 }
---
+++ src/com/panfeng/film/service/PreventRepeat.java
@@ -0,0 +1,7 @@
+package com.panfeng.film.service;
+
+import com.panfeng.film.resource.model.PreventRepeatModel;
+
+public interface PreventRepeat {
+	PreventRepeatModel addPreventRepeat(String token);
+}
