7a4267e350cf82d6880c4cb8a149806ebbc88d39
lutao <694174250@qq.com>
2016-11-03 16:37:25 +0800

    lt

--- src/com/panfeng/film/filter/TokenInterceptor.java
+++ src/com/panfeng/film/filter/TokenInterceptor.java
@@ -4,10 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.panfeng.film.service.PreventRepeat;
 import com.panfeng.film.util.DataUtil;
 import com.panfeng.film.util.ValidateUtil;
 
@@ -19,8 +17,7 @@ import com.panfeng.film.util.ValidateUtil;
  */
 public class TokenInterceptor extends HandlerInterceptorAdapter {
 
-	@Autowired
-	PreventRepeat preventRepeat;
+
 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
