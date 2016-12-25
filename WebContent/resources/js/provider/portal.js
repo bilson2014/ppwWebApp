90ed15952a059d4323370065d0812e55e101eb27
laowang <1300792971@qq.com>
2016-12-25 14:17:29 +0800

    lw

--- WebContent/resources/js/provider/portal.js
+++ WebContent/resources/js/provider/portal.js
@@ -34,8 +34,40 @@ $().ready(function(){
 		$(window.parent.document).find('#tooltip-check').hide();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        							
 	});
 	
+	var msg = $('#recommendation').val();
+	var flag = $("#bean-flag").val();
+	if(flag != null && flag !='' && flag == '0'){
+		//var msg = $('#recommendation').val();
+		showInfomation('您提交的资料正在审核中','官方将在1个工作日内联系您');
+	}else if(flag != null && flag !='' && flag == '2'){
+		showInfomation('您提交的资料审核未通过','审核失败原因：'+msg);
+	}else{
+		var a = $("#bean-checkStatus").val();//是否存在再次审核
+		if(a != null && a !='' && a == '0'){
+			//var msg = $('#recommendation').val();
+			showInfomation('您提交的资料正在审核中','官方将在1个工作日内联系您');
+		}else if(a != null && a !='' && a == '2'){
+			var msg = $('#checkDetails').val();
+			showInfomation('您提交的资料审核未通过','审核失败原因：'+msg);
+		}else{
+			hideInfomation();
+		}
+	}
 	
 });
+
+
+function showInfomation(title,body){
+	$(document).find('#infomation').slideDown();
+	$(document).find('#infomation_title').text(title);
+	$(document).find('#infomation_body').text(body);
+	$(document).find('#closeInfo').on('click',function(){
+		hideInfomation();
+	});
+}
+function hideInfomation(){
+	$(document).find('#infomation').hide();
+}
 
 function getHeight(num){
 	var screen = document.body.clientHeight - 180;
