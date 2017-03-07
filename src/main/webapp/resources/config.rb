7d9d1e1eab0520ede60922e564872a8b45896079
lutao <694174250@qq.com>
2017-03-07 18:12:09 +0800

    lt

--- src/main/webapp/resources/config.rb
+++ src/main/webapp/resources/config.rb
@@ -25,9 +25,11 @@ output_style = (enviroment == :production) ? :compressed : :expanded;
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+
