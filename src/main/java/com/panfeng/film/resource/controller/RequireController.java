d385973e92688b53e93bfca0d36701954b99fda4
lutao <694174250@qq.com>
2017-05-23 15:29:18 +0800

    lt

--- src/main/java/com/panfeng/film/resource/controller/RequireController.java
+++ src/main/java/com/panfeng/film/resource/controller/RequireController.java
@@ -54,44 +54,44 @@ public class RequireController extends BaseController {
 	@RequestMapping("/require/save")
 	public BaseMsg save(final PmsRequire require, Long indentId) {
 		BaseMsg baseMsg = new BaseMsg();
-		baseMsg.setErrorCode(BaseMsg.ERROR);
-		if (ValidateUtil.isValid(indentId)) {
-			PmsIndent indent = pmsIndentFacade.findIndentById(indentId);
-			if (indent != null) {
-				if (require != null) {
-					Map<String, Object> save = pmsRequireFacade.save(require);
-					if (save != null) {
-						Object object = save.get(BaseEntity.SAVE_MAP_ROWS);
-						if (object != null && Integer.valueOf(object.toString()) > 0) {
-							Long requireId = Long.valueOf(save.get(BaseEntity.SAVE_MAP_ID).toString());
-							indent.setRequireId(requireId);
-							pmsIndentFacade.update(indent);
-							baseMsg.setErrorCode(BaseMsg.NORMAL);
-							baseMsg.setErrorMsg("保存成功！");
-							return baseMsg;
-						}
-					}
-					baseMsg.setErrorMsg("保存失败！");
-				} else {
-					baseMsg.setErrorMsg("表单信息错误！");
-				}
-			}
-			baseMsg.setErrorMsg("订单信息不正确！");
-		} else {
-			baseMsg.setErrorMsg("订单信息不能为空！");
-		}
+//		baseMsg.setErrorCode(BaseMsg.ERROR);
+//		if (ValidateUtil.isValid(indentId)) {
+//			PmsIndent indent = pmsIndentFacade.findIndentById(indentId);
+//			if (indent != null) {
+//				if (require != null) {
+//					Map<String, Object> save = pmsRequireFacade.save(require);
+//					if (save != null) {
+//						Object object = save.get(BaseEntity.SAVE_MAP_ROWS);
+//						if (object != null && Integer.valueOf(object.toString()) > 0) {
+//							Long requireId = Long.valueOf(save.get(BaseEntity.SAVE_MAP_ID).toString());
+//							indent.setRequireId(requireId);
+//							pmsIndentFacade.update(indent);
+//							baseMsg.setErrorCode(BaseMsg.NORMAL);
+//							baseMsg.setErrorMsg("保存成功！");
+//							return baseMsg;
+//						}
+//					}
+//					baseMsg.setErrorMsg("保存失败！");
+//				} else {
+//					baseMsg.setErrorMsg("表单信息错误！");
+//				}
+//			}
+//			baseMsg.setErrorMsg("订单信息不正确！");
+//		} else {
+//			baseMsg.setErrorMsg("订单信息不能为空！");
+//		}
 		return baseMsg;
 	}
 
 	@RequestMapping("/require")
 	public ModelAndView requireView(Long indentId, ModelMap model) {
-		model.addAttribute("indentId", indentId);
-		PmsIndent indent = pmsIndentFacade.findIndentById(indentId);
-		Long requireId = indent.getRequireId();
-		if (ValidateUtil.isValid(requireId)) {
-			PmsRequire require = pmsRequireFacade.getRequireInfo(requireId);
-			model.addAttribute("require", require);
-		}
+//		model.addAttribute("indentId", indentId);
+//		PmsIndent indent = pmsIndentFacade.findIndentById(indentId);
+//		Long requireId = indent.getRequireId();
+//		if (ValidateUtil.isValid(requireId)) {
+//			PmsRequire require = pmsRequireFacade.getRequireInfo(requireId);
+//			model.addAttribute("require", require);
+//		}
 		return new ModelAndView("/standardized/requireForm", model);
 	}
 
