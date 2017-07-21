974d3d5ed614bcd4f8f63c84a170bf68719f4681
lirui0 <rui000x@foxmail.com>
2017-07-21 12:05:02 +0800

    seo

--- src/main/java/com/panfeng/film/resource/controller/SolrController.java
+++ src/main/java/com/panfeng/film/resource/controller/SolrController.java
@@ -27,6 +27,8 @@ import com.paipianwang.pat.common.web.domain.ResourceToken;
 import com.paipianwang.pat.common.web.seo.util.SEOUtil;
 import com.paipianwang.pat.facade.information.entity.PmsNewsSolr;
 import com.paipianwang.pat.facade.information.entity.PmsProductSolr;
+import com.paipianwang.pat.facade.product.entity.TdkItem;
+import com.paipianwang.pat.facade.product.service.PmsProductFacade;
 import com.panfeng.film.domain.BaseMsg;
 import com.panfeng.film.resource.view.SolrView;
 import com.panfeng.film.service.SolrService;
@@ -36,6 +38,8 @@ public class SolrController extends BaseController {
 
 	@Autowired
 	private SolrService solrService = null;
+	@Autowired
+	private PmsProductFacade PmsProductFacade=null;
 
 	@RequestMapping("/search")
 	public ModelAndView searchView(String q, final String industry, final String genre, final String production,
@@ -83,16 +87,11 @@ public class SolrController extends BaseController {
 		view.setLimit(20l);
 		
 		//设置tdk
-		JSONObject obj=null;
-		if(ValidateUtil.isValid(target)){
-			obj=SEOUtil.getByKey(target);
-		}else{
-			obj=SEOUtil.getDefault();
-		}
+		TdkItem tdk=PmsProductFacade.getTDKByKey(ValidateUtil.isValid(target)?target:"jpal");
 		
-		model.addAttribute("title",obj==null?"企业宣传视频_产品广告视频_活动视频制作案例大全-拍片网":obj.get(SEOUtil.ITEMS_TITLE_NAME));
-		model.addAttribute("description",obj==null?"企业宣传视频,产品广告视频,活动视频制作案例":obj.get(SEOUtil.ITEMS_DESCRIPTION_NAME));
-		model.addAttribute("keywords",obj==null?"拍片网汇聚各种企业宣传类视频、产品广告类视频以及活动类视频，涵盖各种类型的商业视频案例。":obj.get(SEOUtil.ITEMS_KEYWORDS_NAME));
+		model.addAttribute("title",tdk==null?"企业宣传视频_产品广告视频_活动视频制作案例大全-拍片网":tdk.getTitle());
+		model.addAttribute("description",tdk==null?"企业宣传视频,产品广告视频,活动视频制作案例":tdk.getDescription());
+		model.addAttribute("keywords",tdk==null?"拍片网汇聚各种企业宣传类视频、产品广告类视频以及活动类视频，涵盖各种类型的商业视频案例。":tdk.getKeywords());
 
 		final List<PmsProductSolr> list = solrService.listWithPagination(view, request);
 
