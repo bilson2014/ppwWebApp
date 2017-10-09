cdfda764e233654f8341f820014aacb3475cfe0c
lirui0 <rui000x@foxmail.com>
2017-10-09 17:57:03 +0800

    zhuanti

---
+++ src/main/webapp/resources/salesman/add_js/shangqiao.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?93ab42264ae7c05828fe3f88b039b7a6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
