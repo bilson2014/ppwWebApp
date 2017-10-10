71876c439a6fc0922fe071c2799a4b8b0165f7cc
lirui0 <rui000x@foxmail.com>
2017-10-10 18:26:08 +0800

    zhuanti

--- src/main/webapp/resources/salesman/add_js/shangqiao.js
+++ src/main/webapp/resources/salesman/add_js/shangqiao.js
@@ -1,7 +1,40 @@
+
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?93ab42264ae7c05828fe3f88b039b7a6";
+  hm.src = "https://hm.baidu.com/hm.js?8f5c50c2460f252787da38cc56849189";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+
+
+
+
+$(document).ready(function(e) {
+	
+	$("#menu").click(function(){
+				$(".wap-nav").slideDown();
+			  });
+	$("#close").click(function(){
+				$(".wap-nav").slideUp();
+			  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
