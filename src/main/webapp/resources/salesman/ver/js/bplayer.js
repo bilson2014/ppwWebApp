fc39fc2d02f49a0221705d3482fe5d3f5a9c5b1e
chengxx <chengxiaoxuan110@163.com>
2017-11-24 11:25:46 +0800

    1

--- src/main/webapp/resources/salesman/ver/js/bplayer.js
+++ src/main/webapp/resources/salesman/ver/js/bplayer.js
@@ -1,5 +1,10 @@
 // JavaScript Document
+
 jQuery(function($){
+	
+	
+	
+	
 	var index = 0;
 	var maximg = 23;
 	//$('<div id="flow"></div>').appendTo("#myjQuery");
@@ -7,16 +12,16 @@ jQuery(function($){
 	var MyTime = setInterval(function(){
 		ShowjQueryFlash(index);
 		if(index<maximg){
-			index++;
-		}else if(index>=maximg){
-			index=-1;
-			index++;
-		}
+		index++;}else if(index>=maximg){index=-1;index++}
 	} , 7500);
 });
+
 function ShowjQueryFlash(i) {
-	$("#myJJ li").eq(i).addClass("current").siblings().removeClass("current");
+
+$("#myJJ li").eq(i).addClass("current").siblings().removeClass("current");
+
 }
+
 
 
 
