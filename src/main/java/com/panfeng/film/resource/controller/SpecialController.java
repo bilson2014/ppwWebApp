3972f9e8a9e499fc57d2405578e82a35c4931eeb
lirui0 <rui000x@foxmail.com>
2018-03-29 10:17:42 +0800

    special

---
+++ src/main/java/com/panfeng/film/resource/controller/SpecialController.java
@@ -0,0 +1,28 @@
+package com.panfeng.film.resource.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 专题页处理
+ *
+ */
+@RestController
+public class SpecialController {
+
+	@RequestMapping("/special/{page}.html")
+	public ModelAndView specialPage(@PathVariable("page") final String page,final HttpServletRequest request) {
+
+		return new ModelAndView("/salesman/special/"+page);
+	}
+	
+	@RequestMapping("/special")
+	public ModelAndView indexPage(final HttpServletRequest request) {
+
+		return new ModelAndView("/salesman/special/index");
+	}
+}
